bb72cfedc3a96b40f3a1722b714b261cbf5fddb3
mjlee-29 <achllie@naver.com>
2023-12-02 22:13:37 +0900

    :tada: Set up
    set up

---
+++ src/test/java/api_server/spring_server/SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package api_server.spring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
